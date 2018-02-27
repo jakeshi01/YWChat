a9ec24b4c836f61ba3d7b208e2308136c8371250
jakeshi <jakeshi@meijiabang.cn>
2018-02-27 11:55:16 +0800

    重新初始化

---
+++ YWChat/YWChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YWChat
+//
+//  Created by Jake on 2018/2/27.
+//  Copyright © 2018年 Jake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
