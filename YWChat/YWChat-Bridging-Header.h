d04e75be85db1106c0a28a262cec616da21e7376
jakeshi <jakeshi@meijiabang.cn>
2018-02-27 12:03:49 +0800

    添加桥接文件，引入系统库

---
+++ YWChat/YWChat-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  YWChat-Bridging-Header.h
+//  YWChat
+//
+//  Created by Jake on 2018/2/27.
+//  Copyright © 2018年 Jake. All rights reserved.
+//
+
+#import <WXOpenIMTribeKit/YWTribeKit.h>
+#import <WXOpenIMSDKFMWK/YWFMWK.h>
+#import <WXOUIModule/YWIMKit.h>
+#import <WXOUIModule/YWBaseBubbleViewModel.h>
+#import <WXOUIModule/IYWUIService.h>
+#import <WXOUIModule/YWConversationViewController.h>
+#import <WXOUIModule/YWConversationViewController+BubbleCustomize.h>
+#import <WXOUIModule/YWConversationViewController+BubbleCustomize.h>
+#import <WXOUIModule/YWConversationListViewController.h>
+#import <WXOUIModule/YWConversationViewController.h>
