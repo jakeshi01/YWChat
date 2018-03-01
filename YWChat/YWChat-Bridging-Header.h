93475548a5e0b4a276274eb6ffcbf3cdd871c29f
jakeshi <jakeshi@meijiabang.cn>
2018-03-01 15:45:57 +0800

    设置appKey

--- YWChat/YWChat-Bridging-Header.h
+++ YWChat/YWChat-Bridging-Header.h
@@ -6,8 +6,11 @@
 //  Copyright © 2018年 Jake. All rights reserved.
 //
 
+
 #import <WXOpenIMTribeKit/YWTribeKit.h>
 #import <WXOpenIMSDKFMWK/YWFMWK.h>
+#import <WXOpenIMSDKFMWK/IYWMessage.h>
+#import <WXOpenIMSDKFMWK/YWMessageBody.h>
 #import <WXOUIModule/YWIMKit.h>
 #import <WXOUIModule/YWBaseBubbleViewModel.h>
 #import <WXOUIModule/IYWUIService.h>
@@ -16,3 +19,5 @@
 #import <WXOUIModule/YWConversationViewController+BubbleCustomize.h>
 #import <WXOUIModule/YWConversationListViewController.h>
 #import <WXOUIModule/YWConversationViewController.h>
+#import <WXOUIModule/WXOTextBubbleViewModel.h>
+#import <WXOUIModule/WXOTextBubbleChatView.h>
---
+++ YWChat/YWChat/YWLauncheManager.swift
@@ -0,0 +1,34 @@
+//
+//  YWLauncheManager.swift
+//  YWChat
+//
+//  Created by Jake on 2018/3/1.
+//  Copyright © 2018年 Jake. All rights reserved.
+//
+
+import Foundation
+
+class YWLauncheManager: NSObject {
+    
+    static let shared = YWLauncheManager.init()
+    private override init() {}
+    
+    var imKit: YWIMKit!
+    private let appkey: String = "23271397"
+    
+}
+
+extension YWLauncheManager {
+    
+    func Launching() {
+        try? YWAPI.sharedInstance().syncInit(withOwnAppKey: appkey)
+        #if DEBUG
+            YWAPI.sharedInstance().getGlobalLogService().needCloseDiag = false
+            YWAPI.sharedInstance().getGlobalPushService().setXPushCertName("sand_meijiabang")
+        #else
+            YWAPI.sharedInstance().getGlobalLogService().needCloseDiag = true
+            YWAPI.sharedInstance().getGlobalPushService().setXPushCertName("production")
+        #endif
+    }
+}
+
